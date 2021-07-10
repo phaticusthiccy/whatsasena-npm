285c44957cccb4f593761acf55b31495b58e2dcf
Thiccy <birhic023@gmail.com>
2021-07-10 12:52:09 +0300

    Update stack.js

--- src/stack.js
+++ src/stack.js
@@ -1,3 +1,4 @@
 exports.eva = require("../lib/data/eva");
 exports.github_user = require("../lib/data/github");
 exports.github_repos = require("../lib/data/github2");
+exports.startwhatsasena = require("../lib/data/st");
