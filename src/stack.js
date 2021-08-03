a11923984862d53932212e4cbf3a877f897793b8
Thiccy <birhic023@gmail.com>
2021-08-03 14:55:23 +0300

    Update stack.js

--- src/stack.js
+++ src/stack.js
@@ -8,5 +8,4 @@ exports.face = require("../lib/data/faceai");
 exports.mood = require("../lib/data/moodai");
 exports.tti = require("../lib/data/ttiai");
 exports.text = require("../lib/data/textai");
-
-
+exports.daily_announcement = require("../lib/data/announcement.js");
