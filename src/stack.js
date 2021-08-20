402c9db7b8afd31a56db7b1fe137cd6c02220c6c
Thiccy <birhic023@gmail.com>
2021-08-20 12:29:05 +0300

    Update stack.js

--- src/stack.js
+++ src/stack.js
@@ -20,3 +20,5 @@ exports.ann = require("../lib/data/full_ann");
 exports.datebio = require("../lib/data/date_biography");
 exports.error = require("../lib/data/error_msg");
 exports.search_image = require("../lib/data/img_src");
+exports.anime_wall = require("../lib/data/anime_img");
+exports.anime_gif = require("../lib/data/anime_gif");
