4490ff453563db1a746da67dd5a2e7461ef50979
Thiccy <birhic023@gmail.com>
2021-08-26 11:30:46 +0300

    Update stack.js

--- src/stack.js
+++ src/stack.js
@@ -32,3 +32,4 @@ exports.uploadtest_server = require("../lib/data/uploadtest_server");
 exports.uploadtest_user = require("../lib/data/uploadtest_user");
 exports.speedtest_message = require("../lib/data/speedtest_message");
 exports.speedtest_once = require("../lib/data/speedtest_once");
+exports.github_message = require("../lib/data/github_message");
